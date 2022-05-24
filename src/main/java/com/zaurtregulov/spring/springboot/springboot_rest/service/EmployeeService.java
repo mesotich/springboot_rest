1274b33ab3002a0dfe6cf94db8f49206c70946dc
Dmitry Shishkin <4144442@bk.ru>
2022-05-24 19:19:14 +0300

    SpringBoot-Rest

---
+++ src/main/java/com/zaurtregulov/spring/springboot/springboot_rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.zaurtregulov.spring.springboot.springboot_rest.service;
+
+import com.zaurtregulov.spring.springboot.springboot_rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+}
